142af72e855a1e1c68cd819f1c41eaca520b4340
ATCtech <ankush4singh@gmail.com>
2020-08-18 16:44:54 +0530

    Vibhi

--- cogs/help.py
+++ cogs/help.py
@@ -5,31 +5,67 @@ import random
 from disputils import BotEmbedPaginator
 
 links_str="""
-**-----Youtube-----**
-https://youtube.com/AnkushTechCreator
-**-----Website-----**
-http://ankushtechcreator.com
-**-----GitHub------**
-https://GitHub.com/ATCtech
-**-----Twitter-----**
-https://twitter.com/ATC_YT_2014
-**----Instagram----**
-https://instagram.com/ankush_tech_creator
-**-----Fakebook----**
-https://facebook.com/ankushtechcreator
-**-----Reddit-----**
-https://www.reddit.com/u/TECHIE6023
-**-----Fiverr-----**
-https://fiverr.com/atctech
+[Youtube](https://youtube.com/AnkushTechCreator)
+[Website](http://ankushtechcreator.com)
+[GitHub](https://GitHub.com/ATCtech)
+[Twitter](https://twitter.com/ATC_YT_2014)
+[Instagram](https://instagram.com/ankush_tech_creator)
+[Fakebook](https://facebook.com/ankushtechcreator)
+[Reddit](https://www.reddit.com/u/TECHIE6023)
+[Fiverr](https://fiverr.com/atctech)
 
-If you want your own custom discord bot, join this server and DM @IamL or @GEEKY KID
-**-----Discord-----**
-https://discord.gg/rzJGuWP
+[Discord](https://discord.gg/rzJGuWP)
 
-__Add Vibhi chan to your server using this link__
-https://discord.com/api/oauth2/authorize?client_id=745167619253993543&permissions=536472918&scope=bot
+[Vibhi Chan Invite Link](https://discord.com/api/oauth2/authorize?client_id=745167619253993543&permissions=536472918&scope=bot)
 """
 
+help_str="""
+**__ABOUT__**
+Hi I'm Vibhi, Official mascot of Weebee Con 2020
+Prefix(case sensitive) : ``vibhi``
+
+__**SUPPORT**__
+If you find any bugs or would like to reccomend a feature [join this server](https://discord.gg/rzJGuWP)
+
+__**INVITE**__
+[Invite me to your server](https://discord.com/api/oauth2/authorize?client_id=745167619253993543&permissions=536472918&scope=bot)
+
+__**FUN**__
+  gif
+  meme
+  ask
+  pun
+  joke
+__**ANIME MANGA**__
+  anime
+  animegif
+__**GAMES**__
+  rps (rock paper scissor)
+__**UTILITY**__
+  wiki
+__**MUSIC**__
+  play
+  pause
+  stop
+  skip
+  queue
+  join
+  shuffle
+  remove
+__**MISELLANEOUS**__
+  pfp
+  say
+  invite
+  stats
+  servers
+__**MODERATION**__
+  announce
+  dm
+  clear
+  ban
+  unban
+  kick
+"""
 class Help(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -87,12 +123,17 @@ class Help(commands.Cog):
         ln = discord.Embed(title="Developer Social Links",description=links_str,color=0xFF0055)
         
         
-        
         embeds = [abt,fun,anime,games,music,util,misc,mod,ln]
 
-        paginator = BotEmbedPaginator(ctx, embeds)
-        await paginator.run()
-    
+        #paginator = BotEmbedPaginator(ctx, embeds)
+        #await paginator.run()
+        
+        await ctx.send('Help message has been DMed to you')
+        h = discord.Embed(title='Vibhi Chan help',description=help_str,color=0xFF0055)
+        await ctx.author.send(embed=h)
+        h = discord.Embed(title='Developer Links',description=links_str,color=0xFF0055)
+        await ctx.author.send(embed=h)
+        
 
     
 
