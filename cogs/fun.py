60d2f0ad1341af61ee2481c9ce0a5092a4f2dfa0
ATCtech <ankush4singh@gmail.com>
2020-08-23 16:29:03 +0530

    banned words

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,7 +10,7 @@ import requests
 api_instance = giphy_client.DefaultApi()
 reddit = praw.Reddit(client_id=config('REDDIT_CLIENT_ID'),client_secret=config('REDDIT_CLIENT_SECRET'),user_agent=config('REDDIT_USER_AGENT').replace('-',' '))
 
-banned = ['boob','nigga','nude']
+
 
 class Fun(commands.Cog):
     def __init__(self,bot):
@@ -18,6 +18,7 @@ class Fun(commands.Cog):
         
     @commands.command()
     async def gif(self,ctx):
+        banned = ['boob','nigg','nude','dick','porn','ass','tit','penis']
         q=ctx.message.content[5:].lower()
         for ban in banned:
             if ban in q:
