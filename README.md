81883f362449100e2a12713655f9de7e230ea3b4
ATCtech <ankush4singh@gmail.com>
2020-09-12 20:02:55 +0530

    help cleanup

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Vibhi-Hime
 ![Vibhi](/files/Vibhi.png)\
-Vibhi Hime - The official mascot of Weebee Con 2020 - Indias first Online Anime Con
+Vibhi Hime, a fun featur rich Anime themed discord bot.
+developed by : weeblet~kun#1193
 
 [![](https://img.shields.io/badge/INVITE-VIBHI-ff69b4)](https://discord.com/api/oauth2/authorize?client_id=746984468199374908&permissions=8&scope=bot)
 
@@ -13,7 +14,7 @@ Vibhi Hime - The official mascot of Weebee Con 2020 - Indias first Online Anime 
     - **announce** sends a dm to all members of a guild
     - **dm** sends a dm to the mentioned user
 - **ROLEPLAY**
-    - **bite dance kill run laugh cry punch nosebleed clap** *and many more*
+    - **bite, dance, kill, run, laugh, cry, punch, nosebleed, clap** *and many more*
 - **FUN**
     - **pun**   sends puns
     - **joke**  sends jokes
@@ -28,6 +29,7 @@ Vibhi Hime - The official mascot of Weebee Con 2020 - Indias first Online Anime 
 - **MUSIC**
     - **play**    plays a song
     - **pause**   pauses a song
+    - **resume**  resumes the song
     - **stop**    stops playing music
     - **loop**    loops the currently playing song
     - **join**    makes Vibhi join a Voice Channel
@@ -37,14 +39,17 @@ Vibhi Hime - The official mascot of Weebee Con 2020 - Indias first Online Anime 
     - **skip**    skips a song
 - **UTILITIES**
     - **wiki**  search the wikipedia
+    - **img**   google image search
 - **MISC**
+    - **prefix**   set custom prefixes
+    - **afk**      set afk with custom messages
     - **say**      make the bot say something
     - **avatar**   view the avatar of a user
     - **stats**    view the status of the bot (guild count,latency)
 
 ### Have some suggestions for **Vibhi Chan** ?
 join our server and drop your suggestion and feedbacks\
-[![](https://img.shields.io/discord/695652747487477770?label=DISCORD&logo=discord&logoColor=white)](https://discord.gg/rzJGuWP)
+[![](https://img.shields.io/discord/695652747487477770?label=DISCORD&logo=discord&logoColor=white)](https://discord.gg/7cnnXB)
 
 
 ## Connect with me
@@ -55,7 +60,7 @@ join our server and drop your suggestion and feedbacks\
 [![](https://img.shields.io/badge/-TWITTER-informational?style=flat-square&logo=twitter&logoColor=white&color=blue)](https://twitter.com/ATC_YT_2014/)
 [![](https://img.shields.io/badge/-INSTAGRAM-informational?style=flat-square&logo=instagram&logoColor=white&color=orange)](https://instagram.com/ankush_tech_creator/)
 [![](https://img.shields.io/badge/-FAKEBOOK-informational?style=flat-square&logo=facebook&logoColor=white)](https://facebook.com/ankushtechcreator/)
-[![](https://img.shields.io/discord/695652747487477770?label=DISCORD&logo=discord&logoColor=white)](https://discord.gg/rzJGuWP)
+[![](https://img.shields.io/discord/695652747487477770?label=DISCORD&logo=discord&logoColor=white)](https://discord.gg/7cnnXB)
 [![](https://img.shields.io/badge/-REDDIT-informational?style=flat-square&logo=reddit&logoColor=white&color=black)](https://www.reddit.com/u/TECHIE6023)
 [![](https://img.shields.io/badge/-FIVERR-informational?style=flat-square&logo=fiverr&logoColor=white&color=brightgreen)](https://fiverr.com/atctech)
 
--- cogs/help.py
+++ cogs/help.py
@@ -8,15 +8,8 @@ import json
 from decouple import config
 
 links_str="""
-[Youtube](https://youtube.com/AnkushTechCreator)
-[Website](http://ankushtechcreator.com)
-[GitHub](https://GitHub.com/ATCtech)
-[Twitter](https://twitter.com/ATC_YT_2014)
-[Instagram](https://instagram.com/ankush_tech_creator)
-[Fakebook](https://facebook.com/ankushtechcreator)
-[Reddit](https://www.reddit.com/u/TECHIE6023)
-[Fiverr](https://fiverr.com/atctech)
-[Discord](https://discord.gg/rzJGuWP)
+[Youtube](https://www.youtube.com/channel/UCq4FMXXgsbsZmw5A-Mr7zSA) , [GitHub](https://GitHub.com/ATCtech) , [Twitter](https://twitter.com/ATC_YT_2014) , [Instagram](https://instagram.com/weebletkun) , [Reddit](https://www.reddit.com/u/TECHIE6023) , [Fiverr](https://fiverr.com/atctech)
+[Support Server](https://discord.gg/7cnnXB)
 [Vibhi Chan Invite Link](https://discord.com/api/oauth2/authorize?client_id=746984468199374908&permissions=8&scope=bot)
 """
 
@@ -36,32 +29,25 @@ class Help(commands.Cog):
         try:
             d = db.child("RP").child("GIF").get().val()
             for c in d:
-                rolepl+=c+"   "
+                rolepl+=c+" , "
             
             h = discord.Embed(title='Vibhi Chan help',description='need help?',color=0xFF0055)
-            h.add_field(name='__ABOUT__',value="Hi I'm Vibhi, Official mascot of Weebee Con 2020\nPrefix : ``v!``")
-            h.add_field(name='__SUPPORT__',value="If you find any bugs or would like to reccomend a feature [join this server](https://discord.gg/rzJGuWP)")
+            h.add_field(name='__ABOUT__',value="Hi I'm Vibhi Chan\nPrefix : ``v!``\nFor custom prefix do `v!prefix`\nDeveloped by : ``weeblet~kun#1193``")
+            h.add_field(name='__SUPPORT__',value="If you find any bugs or would like to reccomend a feature [join this server](https://discord.gg/7cnnXB)")
             h.add_field(name='__INVITE__',value="[Invite me to your server (click here)](https://discord.com/api/oauth2/authorize?client_id=746984468199374908&permissions=8&scope=bot)")
-            h.add_field(name='__ROLEPLAY__',value=rolepl)
-            h.add_field(name='__FUN__',value='gif   meme   ask   pun   joke')
-            h.add_field(name='__ANIME MANGA__',value='anime   manga')
+            h.add_field(name='__ROLEPLAY__',value=rolepl[:-3])
+            h.add_field(name='__FUN__',value='gif , meme , ask , pun , joke')
+            h.add_field(name='__ANIME MANGA__',value='anime , manga')
             h.add_field(name='__GAMES__',value='rps')
-            h.add_field(name='__UTILITY__',value='wiki   img')
-            h.add_field(name='__MUSIC__',value='play   pause   resume   stop   skip   queue   join   shuffle   disconnect   remove')
-            h.add_field(name='__MISC__',value='afk   pfp   say   invite   stats   servers   about   prefix')
-            h.add_field(name='__MODERATION__',value='announce   dm   clear   ban   unban   kick')
-            #h.add_field(name='__DEVELOPER LINKS__',value=links_str)
+            h.add_field(name='__UTILITY__',value='wiki , img')
+            h.add_field(name='__MUSIC__',value='play , pause , resume , stop , skip , queue , join , shuffle , disconnect , remove')
+            h.add_field(name='__MISC__',value='afk , pfp , say , invite , stats , servers , about , prefix')
+            h.add_field(name='__MODERATION__',value='announce , dm , clear , ban , unban , kick')
+            h.add_field(name='__DEVELOPER LINKS__',value=links_str)
             #await ctx.author.send(embed=h)
             await ctx.send(embed=h)
         except Exception as e:
             print(e)
-        
-    
-    @commands.command()
-    async def about(self,ctx):
-        h = discord.Embed(title='About Vibhi Chan',description='I am Vibhi, the official mascot of weebee con - Indias first anime con.\nDeveloped by : ``weeblet~kun#1193``',color=0xFF0055)
-        h.add_field(name='__DEVELOPER LINKS__',value=links_str)
-        await ctx.send(embed=h)
     
 
 def setup(bot):
