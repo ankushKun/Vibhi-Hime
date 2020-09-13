f17a0fd3897d50676905274d6c2e028c7b7ea5f9
ATCtech <ankush4singh@gmail.com>
2020-09-13 13:17:57 +0530

    addded roll and toss

--- cogs/games.py
+++ cogs/games.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import random
+from time import sleep as delay
 
 
 class Games(commands.Cog):
@@ -24,7 +25,34 @@ class Games(commands.Cog):
                 await ctx.send('bot wins')
         else:
             await ctx.send('you noob\n``rock``  or  ``paper``  or  ``scissor``?')
-    
+            
+    # COIN TOSS
+    @commands.command()
+    async def toss(self,ctx,side=''):
+        side=side.lower()
+        possibilities=['heads','tails']
+        head=['h','heads','head']
+        tail=['t','tails','tail']
+        await ctx.send('tossing coin . . .')
+        delay(0.5)
+        tossed=random.choice(possibilities)
+        await ctx.send(f'Its **{tossed}**')
+        
+        if side!='':
+            if side in head :side='heads'
+            elif side in tail :side='tails'
+            
+            if side==tossed: await ctx.send('You won')
+            else: await ctx.send('You lost')
+            
+    # ROLL DIE
+    @commands.command()
+    async def roll(self,ctx):
+        await ctx.send("rolling dice . . .")
+        delay(0.5)
+        await ctx.send(f"rolled a **{random.choice(range(1,7))}**")
+        
+        
 
     
 
